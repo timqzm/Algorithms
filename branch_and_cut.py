bdf9eb977ae4e20f683c7e60d7f39865b5d5d7f8
Daniil <krist311@icloud.com>
2017-12-23 22:53:35 +0300

    looks like it works

--- branch_and_cut.py
+++ branch_and_cut.py
@@ -42,14 +42,14 @@ class Painter:
             index = available.index(False)
             coloring[el] = index
             available = [False for i in sorted_indexes]
-        if max(coloring) + 1 == len(nodes):
-            return True  # this is clique
-        else:
-            ind_sets = []
-            for color in range(max(coloring)):
-                if coloring.count(color) < 2:
-                    continue
-                ind_sets.append([index for index, value in enumerate(coloring) if value == color])
+        # if max(coloring) + 1 == len(nodes):
+        #    return True  # this is clique
+        # else:
+        ind_sets = []
+        for color in range(max(coloring)):
+            if coloring.count(color) < 2:
+                continue
+            ind_sets.append([index for index, value in enumerate(coloring) if value == color])
         return ind_sets
 
 
@@ -59,6 +59,7 @@ class BranchAndCut:
         self.degrees = degrees
         self.graph_size = len(graph[0])
         self.current_max_clique_size = 0
+        self.constraints = []
 
     def initializeCplex(self):
         c = cplex.Cplex()
@@ -66,29 +67,48 @@ class BranchAndCut:
         c.variables.add(obj=[1.0] * self.graph_size, ub=[1.0] * self.graph_size,
                         names=['x{0}'.format(x) for x in range(self.graph_size)],
                         types=[c.variables.type.continuous] * self.graph_size)
-        constraints = []
-        for ind_set in Painter(self.graph,self.degrees).paintSetOfVertexes(range(self.graph_size)):
-            constraints.append([['x{0}'.format(x)
-                                 for x in ind_set], [1.0] * len(ind_set)])
-        c.linear_constraints.add(lin_expr=constraints,
-                                 senses=['L'] * len(constraints),
-                                 rhs=[1.0] * len(constraints),
-                                 names=['constr{0}'.format(x) for x in range(len(constraints))])
-        print(constraints)
+
+        for ind_set in Painter(self.graph, self.degrees).paintSetOfVertexes(range(self.graph_size)):
+            self.constraints.append([['x{0}'.format(x) for x in ind_set], [1.0] * len(ind_set)])
+        c.linear_constraints.add(lin_expr=self.constraints,
+                                 senses=['L'] * len(self.constraints),
+                                 rhs=[1.0] * len(self.constraints),
+                                 names=['constr{0}'.format(x) for x in range(len(self.constraints))])
         return c
 
     def get_branching_variable(self, values):
+        max_ = 0
         for i, val in enumerate(values):
-            if not val.is_integer():
-                return i
-        return None
+            if not val.is_integer() and val > max_:
+                max_ = val
+        if max_ != 0:
+            return max_
+        else:
+            return None
 
-    def add_constraint(self, problem: cplex.Cplex, bvar: float, rhs: float):
-        problem.linear_constraints.add(lin_expr=[[[bvar], [1.0]]], senses=['E'], rhs=[rhs],
-                                       names=['bvar_{0}_{1}'.format(bvar, rhs)])
+    def check_clique(self, values):
+        values = sorted(values)
+        for v1 in values:
+            for v2 in values[values.index(v1) + 1:]:
+                if self.graph[v1, v2] == 0:
+                    return tuple([v1, v2])
+                else:
+                    return True
+
+    def add_constraint(self, problem, bvars, rhs, type_eq):
+        if len(bvars) == 1:
+            problem.linear_constraints.add(lin_expr=[[bvars, [1.0]]], senses=[type_eq], rhs=[rhs],
+                                           names=['bvar_{0}_{1}'.format(bvars, rhs)])
+        else:
+            self.constraints.append([['x{0}'.format(x) for x in bvars], [1.0] * len(bvars)])
+            problem.linear_constraints.add(lin_expr=[[['x{0}'.format(x) for x in bvars], [1.0] * len(bvars)]],
+                                           senses=['L'],
+                                           rhs=[1.0],
+                                           names=['constr{0}'.format(len(self.constraints))])
+
         return problem
 
-    def branching(self, problem: cplex.Cplex):
+    def branching(self, problem):
         try:
             problem.solve()
             solution_values = problem.solution.get_values()
@@ -96,14 +116,17 @@ class BranchAndCut:
             return list()
         if sum(solution_values) > self.current_max_clique_size:
             branching_variable = self.get_branching_variable(solution_values)
-            if branching_variable is None:
-                current_clique = list(index for index, value in enumerate(solution_values) if value == 1.0)
-                self.current_max_clique_size = len(current_clique) if self.current_max_clique_size \
-                                                                      < len(
-                    current_clique)else self.current_max_clique_size
-                return current_clique
-            return max(self.branching(self.add_constraint(cplex.Cplex(problem), branching_variable, 1.0)),
-                       self.branching(self.add_constraint(cplex.Cplex(problem), branching_variable, 0.0)),
+            if branching_variable is None: #all solution_values have int type
+                possible_clique= list(index for index, value in enumerate(solution_values) if value == 1.0)
+                check_result = self.check_clique(possible_clique)
+                if check_result is True: # if clique
+                    if self.current_max_clique_size < len(solution_values):
+                        return possible_clique
+                    return list()
+                else:
+                    return self.branching(self.add_constraint(cplex.Cplex(problem), list(check_result), 1.0, 'L'))
+            return max(self.branching(self.add_constraint(cplex.Cplex(problem), [branching_variable], 1.0, 'E')),
+                       self.branching(self.add_constraint(cplex.Cplex(problem), [branching_variable], 0.0, 'E')),
                        key=lambda list: len(list))
         return list()
 
@@ -113,17 +136,18 @@ class BranchAndCut:
 
 def readGraphFromFile(file_name):
     with open(file_name, 'r') as read_file:
+        graph = np.array
         for line in read_file:
-            if line.startswith('p'):
-                num_nodes=int(re.search('[0-9]+', line).group())
-                graph = np.eye(num_nodes)
-                degrees = [0 for x in range(num_nodes)]
-            elif line.startswith('e'):
-                e = re.findall('[0-9]+', line)
-                graph[int(e[0]) - 1][int(e[1]) - 1] = 1
-                graph[int(e[1]) - 1][int(e[0]) - 1] = 1
-                degrees[int(e[0]) - 1]+=1
-                degrees[int(e[1]) - 1]+=1
+            if line[0] == 'p':
+                num_nodes = int(line.split()[2])
+                graph = np.zeros((num_nodes, num_nodes), dtype=bool)
+                degrees = np.zeros(num_nodes)
+            if line[0] == 'e':
+                edge = tuple(int(x) - 1 for x in line[1:].split())
+                graph[edge] = 1
+                graph[edge[1]][edge[0]] = 1
+                degrees[edge[0]] += 1
+                degrees[edge[1]] += 1
 
     return graph, degrees
 
